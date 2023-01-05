5a93e5848a92979be1aaabe11cdc1044fbb07a58
Sergey Sidorov <89048598831s@gmail.com>
2023-01-05 14:49:15 +0300

    ProgrammIsDone

--- Program.cs
+++ Program.cs
@@ -3,3 +3,33 @@
 // задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+using System;
+using static System.Console;
+
+Clear();
+
+string[] first = new string[] { "Hello", "2", "world", ":-)" };
+string[] final = new string[first.Length];
+
+void FillArr(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+void PrintArr(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Write($"{arr[i]} ");
+    }
+    WriteLine();
+}
+FillArr(first, final);
+PrintArr(final);
